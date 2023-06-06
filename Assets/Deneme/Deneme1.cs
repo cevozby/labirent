1ae36a3724ff16acdac017b5665899c8946e5359
cevdetozbey <38431368+cevozby@users.noreply.github.com>
2023-06-06 14:22:56 +0300

    Rigidbody yakalandı

--- Assets/Deneme/Deneme1.cs
+++ Assets/Deneme/Deneme1.cs
@@ -7,7 +7,7 @@ public class Deneme1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
